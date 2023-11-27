9cf51464aa34cc62356c54da7a3a3a873931f31e
Hannguyl870 <Hannguyl870@SDS-L-H8QLG63>
2023-11-27 11:23:11 -0500

    mnvc

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,76 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.Countlable = new System.Windows.Forms.Label();
+            this.colorlable = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.counttimer = new System.Windows.Forms.Timer(this.components);
+            this.timelable = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Countlable
+            // 
+            this.Countlable.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Countlable.Location = new System.Drawing.Point(292, 78);
+            this.Countlable.Name = "Countlable";
+            this.Countlable.Size = new System.Drawing.Size(114, 34);
+            this.Countlable.TabIndex = 0;
+            this.Countlable.Text = "label1";
+            this.Countlable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // colorlable
+            // 
+            this.colorlable.BackColor = System.Drawing.Color.White;
+            this.colorlable.Location = new System.Drawing.Point(309, 127);
+            this.colorlable.Name = "colorlable";
+            this.colorlable.Size = new System.Drawing.Size(78, 59);
+            this.colorlable.TabIndex = 1;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(312, 205);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 2;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // counttimer
+            // 
+            this.counttimer.Interval = 500;
+            this.counttimer.Tick += new System.EventHandler(this.counttimer_Tick);
+            // 
+            // timelable
+            // 
+            this.timelable.Location = new System.Drawing.Point(295, 267);
+            this.timelable.Name = "timelable";
+            this.timelable.Size = new System.Drawing.Size(100, 23);
+            this.timelable.TabIndex = 3;
+            this.timelable.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.timelable);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.colorlable);
+            this.Controls.Add(this.Countlable);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Countlable;
+        private System.Windows.Forms.Label colorlable;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Timer counttimer;
+        private System.Windows.Forms.Label timelable;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,15 +6,64 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Timer_demo
 {
     public partial class Form1 : Form
     {
+        int counter = 0;
+        Stopwatch mywatch = new Stopwatch();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+        
+
+        private void counttimer_Tick(object sender, EventArgs e)
+        {
+            counter++;
+            Countlable.Text = $"{counter}"; 
+           
+            if(counter % 2 ==0)
+            {
+                if (colorlable.BackColor == Color.Red)
+                {
+                    colorlable.BackColor = Color.Red;
+                }
+
+                else if (colorlable.BackColor == Color.Red)
+                {
+                    colorlable.BackColor = Color.Green;
+                }
+                else
+                {
+                    colorlable.BackColor = Color.Red;
+                }
+            }
+           
+        }
+
+       
+        private void start_Click(object sender, EventArgs e)
+        {
+            if(!counttimer.Enabled)
+            {
+                counttimer.Enabled = true;
+                mywatch.Start();
+
+            }
+
+            else
+            {
+                counttimer.Enabled = false;
+               timelable.Text = mywatch.Elapsed.ToString(@"\:ff");
+                //timelable.text = mywatch.elapsedmilliseconds
+                //timelable.text = mywatch.elapsed + "";
+            }
+
         }
     }
 }
